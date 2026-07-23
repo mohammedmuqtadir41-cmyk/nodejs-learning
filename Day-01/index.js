f6aa1fe6e0b7063d9c3c42d4aa785822930fc224
Muqthadir <mohammedmuqtadir41@gmail.com>
2026-07-23 22:46:15 +0530

    feat: add CLI file analyzer with letter, word, and line count

--- Day-01/index.js
+++ Day-01/index.js
@@ -2,8 +2,34 @@ const fs = require("fs");
 
 const path = require("path");
 
-const filePath = path.join(__dirname,"example.txt");
+const filePath = path.join(__dirname, "example.txt");
 
-const data = fs.readFileSync(filePath,"utf8");
+// console.log(fs.readFileSync(filePath,"utf8"));
 
-console.log(data);
+const data = fs.readFileSync(filePath, "utf8");
+
+function letterCount() {
+  console.log("The number of letters are :", data.length);
+}
+
+function wordCount() {
+  console.log("The number of words in the file are:", data.split(" ").length);
+}
+
+function lineCount() {
+  console.log("The number of lines in the file are:", data.split("\n").length);
+}
+
+// lineCount();
+// wordCount();
+// letterCount();
+
+if(process.argv[2] === "letter"){
+    letterCount();
+}
+if(process.argv[2] === "word"){
+    wordCount();
+}
+if(process.argv[2] === "line"){
+    lineCount   ();
+}
