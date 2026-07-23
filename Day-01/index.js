5796bc031409f99cb9c18f36e3249d27e28cc756
Muqthadir <mohammedmuqtadir41@gmail.com>
2026-07-23 22:14:09 +0530

    My first backend code

---
+++ Day-01/index.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+
+const path = require("path");
+
+const filePath = path.join(__dirname,"example.txt");
+
+const data = fs.readFileSync(filePath,"utf8");
+
+console.log(data);
