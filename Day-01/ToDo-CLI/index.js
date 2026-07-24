2f25aeaada0027c0bb1ab251fba2dc87420aeb16
Muqthadir <mohammedmuqtadir41@gmail.com>
2026-07-24 16:51:54 +0530

    Feat: Implemented update and delete function in the todo list using the Commander Library

--- Day-01/ToDo-CLI/index.js
+++ Day-01/ToDo-CLI/index.js
@@ -20,9 +20,39 @@ const addTodo = (new_todo) => {
   console.log("Todo Added Succesfully", new_todo);
 };
 
-// const updateTodo = (update_todo){
-    
-// }
+const updateTodo = (existing_todo, new_todo) => {
+  // const data = fs.readFileSync(filePath, "utf8");
+  // const todoArr = JSON.parse(data);
+
+  // const todoArr = JSON.parse(data);
+  let deletedElementIndex = 0;
+
+  const filterTodo = todoArr.filter((todo, index) => {
+    if (todo.toLowerCase() == existing_todo.toLowerCase()) {
+      deletedElementIndex = index;
+      return false;
+    } else {
+      return true;
+    }
+  });
+
+  filterTodo.splice(deletedElementIndex, 0, new_todo);
+  fs.writeFileSync(filePath, JSON.stringify(filterTodo), "utf8");
+  console.log(`Todo Successfully updated from ${existing_todo} to ${new_todo}`);
+};
+
+const deleteTodo = (todo_val) => {
+  const filtertodo = todoArr.filter((todo) => {
+    if (todo.toLowerCase() == todo_val.toLowerCase()) {
+      return false;
+    } else {
+      return true;
+    }
+  });
+
+  fs.writeFileSync(filePath, JSON.stringify(filtertodo), "utf8");
+  console.log("The todo has been successfully deleted", todo_val);
+};
 
 program
   .command("add")
@@ -38,6 +68,23 @@ program
   .argument("<file_path>", "Argument to print the todo list")
   .action((file_path) => {
     printTodo(file_path);
+  });
+
+program
+  .command("update")
+  .description("To update an existing todo")
+  .argument("<existing_todo>", "Argument for existing todo")
+  .argument("<new_todo>", "To add a new todo")
+  .action((existing_todo, new_todo) => {
+    updateTodo(existing_todo, new_todo);
+  });
+
+program
+  .command("delete")
+  .description("To delete a todo")
+  .argument("<todo_val>", "Argument to delete a todo")
+  .action((todo_val) => {
+    deleteTodo(todo_val);
   });
 
 program.parse();
